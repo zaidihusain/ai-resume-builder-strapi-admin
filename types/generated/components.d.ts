3cb64d6e5655ab799dfc9e7f2a66379c4ee44695
zaidihusain <zaidihusain611@gmail.com>
2024-12-26 05:32:22 +0530

    correcting server side routes

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -12,22 +12,6 @@ export interface SkillsSkills extends Struct.ComponentSchema {
   };
 }
 
-export interface EducationEducation extends Struct.ComponentSchema {
-  collectionName: 'components_education_educations';
-  info: {
-    displayName: 'Education';
-    icon: 'book';
-  };
-  attributes: {
-    universityName: Schema.Attribute.String;
-    degree: Schema.Attribute.String;
-    major: Schema.Attribute.String;
-    startDate: Schema.Attribute.String;
-    endDate: Schema.Attribute.String;
-    description: Schema.Attribute.String;
-  };
-}
-
 export interface ExperienceExperience extends Struct.ComponentSchema {
   collectionName: 'components_experience_experiences';
   info: {
@@ -45,12 +29,28 @@ export interface ExperienceExperience extends Struct.ComponentSchema {
   };
 }
 
+export interface EducationEducation extends Struct.ComponentSchema {
+  collectionName: 'components_education_educations';
+  info: {
+    displayName: 'Education';
+    icon: 'book';
+  };
+  attributes: {
+    universityName: Schema.Attribute.String;
+    degree: Schema.Attribute.String;
+    major: Schema.Attribute.String;
+    startDate: Schema.Attribute.String;
+    endDate: Schema.Attribute.String;
+    description: Schema.Attribute.String;
+  };
+}
+
 declare module '@strapi/strapi' {
   export module Public {
     export interface ComponentSchemas {
       'skills.skills': SkillsSkills;
-      'education.education': EducationEducation;
       'experience.experience': ExperienceExperience;
+      'education.education': EducationEducation;
     }
   }
 }
